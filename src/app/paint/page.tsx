4c519a492101d83dd144d9cf17704dfed77cf4de
Kozikakoo <kozikakoo@yandex.ru>
2024-10-17 12:45:53 +0300

    feat: popup component; context openpopup

--- src/app/paint/page.tsx
+++ src/app/paint/page.tsx
@@ -1,9 +1,11 @@
+import Overlay from "@/components/overlay/overlay";
 import CanvasPaint from "../../components/canvas/canvas";
 import styles from "../page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Overlay>Nt</Overlay>
       <CanvasPaint />
     </main>
   );
